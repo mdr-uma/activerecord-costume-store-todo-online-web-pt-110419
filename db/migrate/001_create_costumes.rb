375aa9348ad058cc369f3d64ca2c47d7d82014bc
Uma Manandhar <um_2003_8@hotmail.com>
2020-02-06 04:05:26 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,7 +11,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.integer :price
+      t.float :price
       t.sring :image_url
       t.string :size
 
