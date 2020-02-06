75e41b458bb65e678eb2cd59108e96b85e11d9fe
Uma Manandhar <um_2003_8@hotmail.com>
2020-02-06 04:09:10 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -12,7 +12,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
     create_table :costumes do |t|
       t.string :name
       t.float :price
-      t.sring :image_url
+      t.string :image_url
       t.string :size
 
       t.timestamps
