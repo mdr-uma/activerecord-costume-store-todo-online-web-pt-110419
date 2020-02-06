91d085905ab324be7b49668c25f59c7faf2f1410
Uma Manandhar <um_2003_8@hotmail.com>
2020-02-06 04:20:30 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -4,7 +4,13 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   def change
     create_table :haunted_houses do |t|
       t.string :name
-      
+      t.string :location
+      t.string :theme
+      t.integer :price
+      t.boolean :family_or_not
+      t.datetime :opening_time
+      t.datetime :closing_time
+      t.text :description
     end
   end
 end
